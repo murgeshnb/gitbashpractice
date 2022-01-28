3cd48136da4f7022e4287158a8c53eef099cea7a
murgeshnb <mnbechetti@gmail.com>
2022-01-28 21:02:30 +0530

    2nd comment for bash

--- src/sample/Testcase04.java
+++ src/sample/Testcase04.java
@@ -4,7 +4,7 @@ public class Testcase04 {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Test case 4 for bash clone");
+		System.out.println("Test case 4 for bash clone..");
 
 	}
 
