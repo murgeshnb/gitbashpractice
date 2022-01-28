3d6e352b8c331a5757b3b62ebfdc964afd0bcb4f
murgeshnb <mnbechetti@gmail.com>
2022-01-28 19:33:53 +0530

    Firstbranch committing message

--- src/sample/Simple.java
+++ src/sample/Simple.java
@@ -3,7 +3,6 @@ package sample;
 public class Simple {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		
 		System.out.println("hi");
 		System.out.println("heyyyy modified");
---
+++ src/sample/Testcase03.java
@@ -0,0 +1,11 @@
+package sample;
+
+public class Testcase03 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("For branching 1");
+
+	}
+
+}
